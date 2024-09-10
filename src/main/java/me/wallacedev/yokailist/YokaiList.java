a8a7b8133d5c2f1d4fb9c83a3bbd68dc48ae82a7
yokaithefato <wallacewwc2015wwk@gmail.com>
2024-09-10 17:41:43 -0300

    first commit

---
+++ src/main/java/me/wallacedev/yokailist/YokaiList.java
@@ -0,0 +1,13 @@
+package me.wallacedev.yokailist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YokaiList {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YokaiList.class, args);
+	}
+
+}
---
+++ src/test/java/me/wallacedev/todo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.wallacedev.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
